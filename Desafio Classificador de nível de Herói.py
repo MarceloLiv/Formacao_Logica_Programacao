c1e55ebe665ed2a5a6cd9b11687b202701e2bfa5
MarceloLiv <marcelolourenco7@gmail.com>
2025-03-30 17:08:46 -0300

    Adicionando o script classificador de heroi

---
+++ Desafio Classificador de nível de Herói.py
@@ -0,0 +1,46 @@
+def classificar_heroi(nome, xp):
+    if xp < 1000:
+        nivel = "Ferro"
+    elif xp <= 2000:
+        nivel = "Bronze"
+    elif xp <= 5000:
+        nivel = "Prata"
+    elif xp <= 7000:
+        nivel = "Ouro"
+    elif xp <= 8000:
+        nivel = "Platina"
+    elif xp <= 9000:
+        nivel = "Ascendente"
+    elif xp <= 10000:
+        nivel = "Imortal"
+    else:
+        nivel = "Radiante"
+    
+    print(f"O Herói de nome {nome} está no nível de {nivel}")
+
+# Lista de heróis e seus XP
+herois = [
+    ("Goku", 500),
+    ("Chapolin", 1500),
+    ("Hulk", 3000),
+    ("Batman", 6000),
+    ("Superman", 7500),
+    ("Pegasus", 8500),
+    ("Andromeda", 9500),
+    ("Fenix", 11000)
+]
+
+# Exibir opções de heróis
+print("Escolha um herói:")
+for i, (nome, _) in enumerate(herois, 1):
+    print(f"{i}. {nome}")
+
+# Solicitar escolha do usuário
+escolha = int(input("Digite o número do herói escolhido: ")) - 1
+
+# Validar escolha e classificar
+if 0 <= escolha < len(herois):
+    nome_heroi, xp_heroi = herois[escolha]
+    classificar_heroi(nome_heroi, xp_heroi)
+else:
+    print("Escolha inválida!")
